46933d0e6a01bebe39d5091e87a827b27affd30d
Muzzammil Islam <muzzammil424221@gmail.com>
2024-06-23 12:00:58 +0530

    changes in category

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get('/search/:key',async(req,resp)=>{
         {
             "$or":[
                 {"name":{$regex:req.params.key}},
-                {"brand":{$regex:req.params.key}}
+                {"brand":{$regex:req.params.key}},
+                {"category":{$regex:req.params.key}}
             ]
         }
     );
